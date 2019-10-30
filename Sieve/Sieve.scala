83d40e128cacaae6c27d04c0836aa07283adc6f0
yaooo <yaoshi616@gmail.com>
2019-10-30 19:25:59 -0400

    Start sieve

---
+++ Sieve/Sieve.scala
@@ -0,0 +1,98 @@
+/** A sequential implementation of the Sieve of Eratosthenes */
+
+import java.util.concurrent.atomic.AtomicIntegerArray
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicBoolean
+
+object Sieve {
+
+    def sequential(N: Int) = {
+        val primes = new Array[Int](N) // will hold the primes
+        primes(0) = 2
+        var nextSlot = 1 // next free slot in primes
+        var next = 3 // next candidate prime to consider
+
+        while (nextSlot < N) {
+            // Test if next is prime;
+            // invariant: next is coprime with primes[0..i) && p = primes(i)
+            var i = 0;
+            var p = primes(i)
+            while (p * p <= next && next % p != 0) {
+                i += 1; p = primes(i)
+            }
+            if (p * p > next) { // next is prime
+                primes(nextSlot) = next;
+                nextSlot += 1
+            }
+            next += 1
+        }
+
+        println(primes(N - 1))
+    }
+
+  def insertPrime(primes: AtomicIntegerArray, prime: Int, start: Int): Boolean = {
+    var i = start
+    var p = prime
+
+    while (p != 0) {
+      var cur = primes.get(i)
+      while (cur > 0 && cur < p) {
+        i += 1
+        if (i >= primes.length) return false
+        cur = primes.get(i)
+      }
+      // find the position to insert the number
+
+      if (primes.compareAndSet(i, cur, p)) {
+        p = cur 
+      } 
+    }
+    true
+  }
+
+    def isPrime(n: Int): Boolean = ! ((2 until scala.math.sqrt(n).toInt) exists (n % _ == 0))
+
+
+    def concurrent(N: Int, int par) = {
+        val primes = new Array[Int](N)
+        primes(0) = 2
+        val current = new AtomicIntegerArray[Int](par)   //current ids for the threads
+        val next = new AtomicInteger(3) //next long to be tested
+        var nextSlot = new AtomicInteger(1) // index of the next prime that should be inserted
+
+        def solve(id: Int)={
+            while(primes.get(N-1) == 0){
+                var cur_number = next.getAndIncrement()
+                var start = nextSlot.get()
+
+                current(id) = cur_number
+
+                // spining lock, spin if "(m ^ 2 <= n)" (overflow??)
+                var valid = false
+                while(!valid){
+                    valid = true
+                    for(i <- 0 until par){
+                        valid &= !(current(i) <= Math.sqrt(cur_number))
+                    }
+                }
+
+
+            }
+
+        }
+
+        ox.cads.util.ThreadUtil.runIndexedSystem(par, solve)
+        println(primes.get(N - 1))
+    }
+
+
+    def main(args: Array[String]) = {
+        assert(args.length == 1, "must have one argument")
+
+        val N = args(0).toInt // number of primes required
+
+        val t0 = java.lang.System.currentTimeMillis()
+        sequential(N)
+        println("Time taken: " + (java.lang.System.currentTimeMillis() - t0))
+    }
+}
--- sudoku/ConcurrentSudoku.scala
+++ sudoku/ConcurrentSudoku.scala
@@ -1,3 +1,20 @@
+/*Conclusion:
+
+To solve the sudoko 100 times:
+  When using simple solver (without "-a"), the sequential solver takes around 16000 ms.
+  When using simple solver (without "-a"), the concurrent solver (10 threads) takes around 7723 ms.
+
+
+  When using the advanced partial (with "-a"), the sequential solver takes around 1143 ms.
+  When using the advanced partial (with "-a"), the concurrent solver (10 threads) takes around 1106 ms.
+
+Without the advanced partial, it greatly reduced the overall time using the concurrent solver
+With the advanced partial, because the tree is traversed in a better way, the overall time does not have much effects regarding these two approaches.
+
+*/
+
+
+
 /** Sudoku solver.
   * Basic usage:
   * # scala Sudoku filename: solves the puzzle in filename
@@ -6,6 +23,8 @@
   * Options:
   * # -n n: repeat n times
   * # -a: use the AdvancedPartial
+  * # -p: the number of threads
+  * # -s: the section of the problem, 0(sequential), 1(problem 1), 2(problem 2), 3(problem 3)
   */
 import ox.cads.util.Profiler
 import java.util.concurrent.atomic.AtomicBoolean
@@ -13,6 +32,8 @@ import ox.cads.collection.TerminationDetectingPool
 import ox.cads.collection.Pool
 import scala.collection.mutable.ArrayBuffer
 
+
+// extrend the traint pool for the object of the TerminationDetectingPool
 class StackPool[T] extends Pool[T]{
   private var stack = new ox.cads.collection.LockFreeStack[T]
 
@@ -57,6 +78,121 @@ object ConcurrentSudoku {
     } // end of while
   }
 
+//   scala Sudoku --all -s 1 -p 10
+// test1.sud
+// 643287951
+// 592316487
+// 817459263
+// 981634725
+// 235798146
+// 764521398
+// 456973812
+// 328145679
+// 179862534
+
+// test2.sud
+// 987645321
+// 654321978
+// 312987654
+// 896734512
+// 745219863
+// 231568497
+// 578193246
+// 469872135
+// 123456789
+
+// test3.sud
+// 987654321
+// 654321987
+// 321987654
+// 896745213
+// 745213896
+// 213896745
+// 579468132
+// 468132579
+// 132579468
+
+// test4.sud
+// 943867251
+// 582913467
+// 617452983
+// 891634725
+// 235798146
+// 764521398
+// 459376812
+// 328145679
+// 176289534
+
+// test5.sud
+// 956431827
+// 748296351
+// 213785469
+// 589612734
+// 674953218
+// 321847596
+// 492568173
+// 167324985
+// 835179642
+
+// test6.sud
+// 731245869
+// 642891357
+// 589763421
+// 425378196
+// 813629574
+// 976154283
+// 357486912
+// 294517638
+// 168932745
+
+// test7.sud
+// 465237891
+// 219658374
+// 783149652
+// 928563417
+// 546791238
+// 137824569
+// 372986145
+// 651472983
+// 894315726
+
+// test8.sud
+// 937162485
+// 581493276
+// 624587913
+// 213648759
+// 479325168
+// 865971324
+// 752839641
+// 196754832
+// 348216597
+
+// test9.sud
+// 352617948
+// 468329517
+// 179584263
+// 813265479
+// 295741386
+// 647938152
+// 731496825
+// 986152734
+// 524873691
+
+// test10.sud
+// 731986245
+// 842753169
+// 695142837
+// 384561972
+// 917234586
+// 256879314
+// 523417698
+// 178695423
+// 469328751
+
+// impossible.sud
+// Time taken: 174
+
+
   /** question 1 **/
   def solve1(init: Partial, par: Int) {
     // Stack to store partial solutions that we might back-track to.
@@ -69,15 +205,16 @@ object ConcurrentSudoku {
         stack.pop match {
           case None =>{}
           case Some(partial)=>{
-            val complete = partial.complete
+            val complete = partial.complete // if the partial solution is complete
             if(complete && done.compareAndSet(false, true)){
               partial.printPartial
             }else if(!complete){
+              // Choose position to play
               val (i, j) = partial.nextPos;
+              // Consider all values to play there
               for (d <- 1 to 9)
                 if (partial.canPlay(i, j, d)) {
-                  val p1 = partial.play(i, j, d)
-                  if(!done.get) stack.push(p1)
+                  val p1 = partial.play(i, j, d); stack.push(p1)
                 }
             }
           } 
@@ -88,6 +225,123 @@ object ConcurrentSudoku {
   }
 
 
+
+
+
+// scala Sudoku --all -s 2 -p 10
+// test1.sud
+// 643287951
+// 592316487
+// 817459263
+// 981634725
+// 235798146
+// 764521398
+// 456973812
+// 328145679
+// 179862534
+
+// test2.sud
+// 987645321
+// 654321978
+// 312987654
+// 896734512
+// 745219863
+// 231568497
+// 578193246
+// 469872135
+// 123456789
+
+// test3.sud
+// 987654321
+// 654321987
+// 321987654
+// 896745213
+// 745213896
+// 213896745
+// 579468132
+// 468132579
+// 132579468
+
+// test4.sud
+// 943867251
+// 582913467
+// 617452983
+// 891634725
+// 235798146
+// 764521398
+// 459376812
+// 328145679
+// 176289534
+
+// test5.sud
+// 956431827
+// 748296351
+// 213785469
+// 589612734
+// 674953218
+// 321847596
+// 492568173
+// 167324985
+// 835179642
+
+// test6.sud
+// 731245869
+// 642891357
+// 589763421
+// 425378196
+// 813629574
+// 976154283
+// 357486912
+// 294517638
+// 168932745
+
+// test7.sud
+// 465237891
+// 219658374
+// 783149652
+// 928563417
+// 546791238
+// 137824569
+// 372986145
+// 651472983
+// 894315726
+
+// test8.sud
+// 937162485
+// 581493276
+// 624587913
+// 213648759
+// 479325168
+// 865971324
+// 752839641
+// 196754832
+// 348216597
+
+// test9.sud
+// 352617948
+// 468329517
+// 179584263
+// 813265479
+// 295741386
+// 647938152
+// 731496825
+// 986152734
+// 524873691
+
+// test10.sud
+// 731986245
+// 842753169
+// 695142837
+// 384561972
+// 917234586
+// 256879314
+// 523417698
+// 178695423
+// 469328751
+
+// impossible.sud
+// Time taken: 175
+
   /** question 2 **/
   def solve2(init: Partial, par: Int) {
     // Stack to store partial solutions that we might back-track to.
@@ -96,15 +350,16 @@ object ConcurrentSudoku {
       par
     )
     val done = new AtomicBoolean(false)
-
-    // Stack to store partial solutions that we might back-track to.
     pool.add(init)
 
+// • a value can be returned;
+// • all p threads are attempting a get, at which point all return None;
+// • another thread calls the signalDone operation, at which point all
+// threads attempting a get return None.
     def solver() {
       while (!done.get) {
-        // pool.get will spin until pool is not empty or some thread invoke signalDone.
         pool.get match {
-          // Return immediate upon one unsuccessful get.
+          // return for gettin a None
           case None => { return }
           case Some(partial) => {
             val complete = partial.complete
@@ -129,20 +384,42 @@ object ConcurrentSudoku {
   }
 
 
+
+
+// scala Sudoku --all -s 3 -p 8 -n 100
+// Time taken: 9151
+
+// scala Sudoku --all -s 3 -p 10 -n 100
+// Time taken: 16551
+
+
+// scala Sudoku --all -s 3 -p 4 -n 100
+// Time taken: 16775
+
+// scala Sudoku --all -s 3 -p 4 -a -n 100
+// Time taken: 1318
+
+
+// scala Sudoku --all -s 3 -p 40 -n 50 -a
+// Time taken: 750
+
+// scala Sudoku --all -s 3 -p 40 -n 50
+// Time taken: 4632
+
   /** question 3 **/
   // To optimize the stack, we try not to have so many workers at the same time because they might just block each other.
   // We reduced the number of workers that are needed in order to fully use the resources 
   // "Amdahl's law"
+
+  // Or we can just ust "-a" option which pushes the partial solution with the least variations into the stack
+  // In this case, we push fewer possible partial solutions to the stack, thus reduced the stack size
   def solve3(init: Partial, par: Int) {
     // Stack to store partial solutions that we might back-track to.
     var num = par
     val cores = Runtime.getRuntime().availableProcessors();
     if(cores < par)
-      num = cores/3*2
+      num = cores
     println("Number of threads:" + num)
-    // if(par > cores){
-    //   par = cores
-    // }
 
     val pool = new TerminationDetectingPool[Partial](
       new StackPool[Partial],
@@ -208,7 +485,7 @@ object ConcurrentSudoku {
     var section = 0 // the section of the problem, ex: 1,2,3
     var count = 1 // number of tests
     var fname = "" // filename
-    var adv = true // are we using the AdvancedPartial?
+    var adv = false // are we using the AdvancedPartial?
     var all = false // are we doing all files in allFiles?
     var allPoss = false // are we doing all files in allPossibleFiles?
     // parse command line arguments
@@ -269,6 +546,6 @@ object ConcurrentSudoku {
     
 
     println("Time taken: " + (System.currentTimeMillis() - t0))
-    Profiler.report
+    // Profiler.report
   }
 }
